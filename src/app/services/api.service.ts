26d7216501a4b0899c418349eb2b334977ed0ddd
Hallison Brancalhao <hallison.o.b@gmail.com>
2023-12-01 16:35:51 -0300

    feat: criando requisições em

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject, signal } from '@angular/core';
-import { Person, Response } from '@shared/data-access';
+import {
+  CreatePersonDto,
+  Person,
+  Response,
+  UpdatePersonDto,
+} from '@shared/data-access';
 import { Observable, shareReplay, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -11,9 +16,15 @@ export class ApiService {
   #http = inject(HttpClient);
   #url = signal(environment.apiUrl);
 
+  #person = signal<Person | null>(null);
   #persons = signal<Person[] | null>(null);
+
   get personsList() {
     return this.#persons.asReadonly();
+  }
+
+  get person() {
+    return this.#person.asReadonly();
   }
 
   personList$(): Observable<Response<Person>> {
@@ -22,5 +33,24 @@ export class ApiService {
       shareReplay(),
       tap((persons) => this.#persons.set(persons.results))
     );
+  }
+
+  person$(id: number): Observable<Person> {
+    return this.#http.get<Person>(`${this.#url()}/${id}`).pipe(
+      shareReplay(),
+      tap((person) => this.#person.set(person))
+    );
+  }
+
+  create(person: CreatePersonDto) {
+    return this.#http.post(this.#url(), person);
+  }
+
+  update(id: string, person: UpdatePersonDto) {
+    return this.#http.patch(`${this.#url()}/${id}`, { person });
+  }
+
+  delete(id: string) {
+    return this.#http.delete(`${this.#url()}/${id}`);
   }
 }
--- src/app/shared/data-access/public-api.ts
+++ src/app/shared/data-access/public-api.ts
@@ -1,3 +1,4 @@
 export * from './interfaces';
 export * from './entities';
 export * from './types';
+export * from './dtos';
